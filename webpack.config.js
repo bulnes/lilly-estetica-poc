042ba7890c9f9b9421e22a96f2fc8840f9246435
Bruno Bulnes <bulnes.bruno@gmail.com>
2024-02-25 22:42:12 -0300

    feat: add minified optimization

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,13 @@
 const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+const HtmlMinimizerPlugin = require("html-minimizer-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
+    clean: true,
   },
   devServer: {
     static: {
@@ -12,5 +15,22 @@ module.exports = {
     },
     compress: true,
     port: 9000,
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: "public" }],
+    }),
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [new HtmlMinimizerPlugin()],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        type: "asset/resource",
+      },
+    ],
   },
 };
